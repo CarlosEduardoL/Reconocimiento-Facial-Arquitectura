234ada4e4855b7b9cee481e417c444285ba05a2c
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2019-11-27 08:08:06 -0500

    programa arreglado

--- tf_python/image/LBP.py
+++ tf_python/image/LBP.py
@@ -8,70 +8,120 @@ class LBP:
     def __init__(self, image_route):
         self.image_route = image_route
         self.image = np.array(io.imread(image_route, as_gray=True) * 255, dtype=np.uint8)
-        self.__convert = 1 << np.array([i for i in range(7, -1, -1)], dtype=np.uint8)
-        self.mask8_1 = np.array([[1, 2, 4],
-                                 [128, 0, 8],
-                                 [64, 32, 16]])
-        self.mask8_2 = np.array([[1, 0, 2, 0, 4],
-                                 [0, 0, 0, 0, 0],
-                                 [128, 0, 0, 0, 8],
-                                 [0, 0, 0, 0, 0],
-                                 [64, 0, 32, 0, 16]])
-        self.mask16_2 = np.array([[1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4],
-                                  [1 << 15, 0, 0, 0, 1 << 5],
-                                  [1 << 14, 0, 0, 0, 1 << 6],
-                                  [1 << 13, 0, 0, 0, 1 << 7],
-                                  [1 << 12, 1 << 11, 1 << 10, 1 << 9, 1 << 8]])
+        self.lbp_image = np.zeros((self.image.shape[0] - 2, self.image.shape[1] - 2), dtype=np.uint16)
 
     def lbp_image_his_8_1ij(self):
-        lbp_image = np.zeros((self.image.shape[0] - 2, self.image.shape[1] - 2), dtype=np.uint8)
         for i in range(2, self.image.shape[0] - 2):
             for j in range(2, self.image.shape[1] - 2):
-                temp = self.image[i - 1:i + 2, j - 1:j + 2]
-                lbp_image[i - 2, j - 2] = sum(sum(np.array(temp < temp[1, 1], dtype=np.int8) * self.mask8_1))
-        return lbp_image
+                mask: np.uint8 = np.uint8(0)
+                pixel: np.uint8 = self.image[i, j]
+                mask |= (self.image[i - 1, j - 1] < pixel) << 7
+                mask |= (self.image[i, j - 1] < pixel) << 6
+                mask |= (self.image[i + 1, j - 1] < pixel) << 5
+                mask |= (self.image[i + 1, j] < pixel) << 4
+                mask |= (self.image[i + 1, j + 1] < pixel) << 3
+                mask |= (self.image[i, j + 1] < pixel) << 2
+                mask |= (self.image[i - 1, j + 1] < pixel) << 1
+                mask |= (self.image[i - 1, j] < pixel) << 0
+                self.lbp_image[i - 2, j - 2] = mask
+        return self.lbp_image
 
     def lbp_image_his_8_1ji(self):
-        lbp_image = np.zeros((self.image.shape[0] - 2, self.image.shape[1] - 2), dtype=np.uint8)
         for j in range(2, self.image.shape[1] - 2):
             for i in range(2, self.image.shape[0] - 2):
-                temp = self.image[i - 1:i + 2, j - 1:j + 2]
-                lbp_image[i - 2, j - 2] = sum(sum(np.array(temp < temp[1, 1], dtype=np.int8) * self.mask8_1))
-        return lbp_image
+                mask: np.uint8 = np.uint8(0)
+                pixel: np.uint8 = self.image[i, j]
+                mask |= (self.image[i - 1, j - 1] < pixel) << 7
+                mask |= (self.image[i, j - 1] < pixel) << 6
+                mask |= (self.image[i + 1, j - 1] < pixel) << 5
+                mask |= (self.image[i + 1, j] < pixel) << 4
+                mask |= (self.image[i + 1, j + 1] < pixel) << 3
+                mask |= (self.image[i, j + 1] < pixel) << 2
+                mask |= (self.image[i - 1, j + 1] < pixel) << 1
+                mask |= (self.image[i - 1, j] < pixel) << 0
+                self.lbp_image[i - 2, j - 2] = mask
+        return self.lbp_image
 
     def lbp_image_his_8_2ij(self):
-        lbp_image = np.zeros((self.image.shape[0] - 2, self.image.shape[1] - 2), dtype=np.uint8)
         for i in range(2, self.image.shape[0] - 2):
             for j in range(2, self.image.shape[1] - 2):
-                temp = self.image[i - 2:i + 3, j - 2:j + 3]
-                lbp_image[i - 2, j - 2] = sum(sum(np.array(temp < temp[1, 1], dtype=np.int8) * self.mask8_2))
-        return lbp_image
+                mask: np.uint8 = np.uint8(0)
+                pixel: np.uint8 = self.image[i, j]
+                mask |= (self.image[i - 2, j - 2] < pixel) << 7
+                mask |= (self.image[i, j - 2] < pixel) << 6
+                mask |= (self.image[i + 2, j - 2] < pixel) << 5
+                mask |= (self.image[i + 2, j] < pixel) << 4
+                mask |= (self.image[i + 2, j + 2] < pixel) << 3
+                mask |= (self.image[i, j + 2] < pixel) << 2
+                mask |= (self.image[i - 2, j + 2] < pixel) << 1
+                mask |= (self.image[i - 2, j] < pixel) << 0
+                self.lbp_image[i - 2, j - 2] = mask
+        return self.lbp_image
 
     def lbp_image_his_8_2ji(self):
-        lbp_image = np.zeros((self.image.shape[0] - 1, self.image.shape[1] - 1), dtype=np.uint8)
         for j in range(2, self.image.shape[1] - 2):
             for i in range(2, self.image.shape[0] - 2):
-                temp = self.image[i - 2:i + 3, j - 2:j + 3]
-                lbp_image[i - 2, j - 2] = sum(sum(np.array(temp < temp[2, 2], dtype=np.int8) * self.mask8_2))
-        return lbp_image
+                mask: np.uint8 = np.uint8(0)
+                pixel: np.uint8 = self.image[i, j]
+                mask |= (self.image[i - 2, j - 2] < pixel) << 7
+                mask |= (self.image[i, j - 2] < pixel) << 6
+                mask |= (self.image[i + 2, j - 2] < pixel) << 5
+                mask |= (self.image[i + 2, j] < pixel) << 4
+                mask |= (self.image[i + 2, j + 2] < pixel) << 3
+                mask |= (self.image[i, j + 2] < pixel) << 2
+                mask |= (self.image[i - 2, j + 2] < pixel) << 1
+                mask |= (self.image[i - 2, j] < pixel) << 0
+                self.lbp_image[i - 2, j - 2] = mask
+        return self.lbp_image
 
     def lbp_image_his_16_2ij(self):
-        lbp_image = np.zeros((self.image.shape[0] - 2, self.image.shape[1] - 2), dtype=np.uint8)
         for i in range(2, self.image.shape[0] - 2):
             for j in range(2, self.image.shape[1] - 2):
-                temp = self.image[i - 2:i + 3, j - 2:j + 3]
-                lbp_image[i - 2, j - 2] = sum(sum(np.array(temp < temp[1, 1], dtype=np.int8) * self.mask16_2))
-        return lbp_image
+                mask: np.uint16 = np.uint16(0)
+                pixel: np.uint8 = self.image[i, j]
+                mask |= (self.image[i - 2, j - 1] < pixel) << 15
+                mask |= (self.image[i - 2, j - 2] < pixel) << 14
+                mask |= (self.image[i - 1, j - 2] < pixel) << 13
+                mask |= (self.image[i, j - 2] < pixel) << 12
+                mask |= (self.image[i + 1, j - 2] < pixel) << 11
+                mask |= (self.image[i + 2, j - 2] < pixel) << 10
+                mask |= (self.image[i + 2, j - 1] < pixel) << 9
+                mask |= (self.image[i + 2, j] < pixel) << 8
+                mask |= (self.image[i + 2, j + 1] < pixel) << 7
+                mask |= (self.image[i + 2, j + 2] < pixel) << 6
+                mask |= (self.image[i + 1, j + 2] < pixel) << 5
+                mask |= (self.image[i, j + 2] < pixel) << 4
+                mask |= (self.image[i - 1, j + 2] < pixel) << 3
+                mask |= (self.image[i - 2, j + 2] < pixel) << 2
+                mask |= (self.image[i - 2, j + 1] < pixel) << 1
+                mask |= (self.image[i - 2, j] < pixel) << 0
+                self.lbp_image[i - 2, j - 2] = mask
+        return self.lbp_image
 
     def lbp_image_his_16_2ji(self):
-        lbp_image = np.zeros((self.image.shape[0] - 1, self.image.shape[1] - 1), dtype=np.uint16)
         for j in range(2, self.image.shape[1] - 2):
             for i in range(2, self.image.shape[0] - 2):
-                temp = self.image[i - 2:i + 3, j - 2:j + 3]
-                lbp_image[i - 2, j - 2] = sum(sum(np.array(temp < temp[2, 2], dtype=np.int16) * self.mask16_2))
-        return lbp_image
+                mask: np.uint16 = np.uint16(0)
+                pixel: np.uint8 = self.image[i, j]
+                mask |= (self.image[i - 2, j - 1] < pixel) << 15
+                mask |= (self.image[i - 2, j - 2] < pixel) << 14
+                mask |= (self.image[i - 1, j - 2] < pixel) << 13
+                mask |= (self.image[i, j - 2] < pixel) << 12
+                mask |= (self.image[i + 1, j - 2] < pixel) << 11
+                mask |= (self.image[i + 2, j - 2] < pixel) << 10
+                mask |= (self.image[i + 2, j - 1] < pixel) << 9
+                mask |= (self.image[i + 2, j] < pixel) << 8
+                mask |= (self.image[i + 2, j + 1] < pixel) << 7
+                mask |= (self.image[i + 2, j + 2] < pixel) << 6
+                mask |= (self.image[i + 1, j + 2] < pixel) << 5
+                mask |= (self.image[i, j + 2] < pixel) << 4
+                mask |= (self.image[i - 1, j + 2] < pixel) << 3
+                mask |= (self.image[i - 2, j + 2] < pixel) << 2
+                mask |= (self.image[i - 2, j + 1] < pixel) << 1
+                mask |= (self.image[i - 2, j] < pixel) << 0
+                self.lbp_image[i - 2, j - 2] = mask
+        return self.lbp_image
 
     @staticmethod
     def make_global_histogram(lbp_image: np.ndarray):
         return np.unique(lbp_image, return_counts=True)[1]
-
--- tf_python/image/experiment.py
+++ tf_python/image/experiment.py
@@ -2,9 +2,9 @@ import time
 from image.LBP import LBP
 
 print('Staring')
-for repetition in range(0, 105):
-    with open('data' + str(repetition) + '.csv', 'a+')as file:
-        file.write('tamanho,algoritmo,localidad,tiempo\n')
+with open('data.csv', 'a+')as file:
+    for repetition in range(0, 110):
+        file.write('repeticion, tamanho,algoritmo,localidad,tiempo\n')
         file.flush()
         for running in range(1, 5):
             lbp = LBP('../images/level' + str(running) + '.png')
@@ -12,28 +12,28 @@ for repetition in range(0, 105):
 
             t = time.time()
             lbp.lbp_image_his_8_1ij()
-            file.write(str(running) + ',8_1,si,' + str(time.time() - t) + '\n')
+            file.write(str(repetition) + ',' + str(running) + ',8_1,si,' + str(time.time() - t) + '\n')
             file.flush()
             t = time.time()
             lbp.lbp_image_his_8_2ij()
-            file.write(str(running) + ',8_2,si,' + str(time.time() - t) + '\n')
+            file.write(str(repetition) + ',' + str(running) + ',8_2,si,' + str(time.time() - t) + '\n')
             file.flush()
             t = time.time()
             lbp.lbp_image_his_16_2ij()
-            file.write(str(running) + ',16_2,si,' + str(time.time() - t) + '\n')
+            file.write(str(repetition) + ',' + str(running) + ',16_2,si,' + str(time.time() - t) + '\n')
             file.flush()
 
             t = time.time()
             lbp.lbp_image_his_8_1ji()
-            file.write(str(running) + ',8_1,no,' + str(time.time() - t) + '\n')
+            file.write(str(repetition) + ',' + str(running) + ',8_1,no,' + str(time.time() - t) + '\n')
             file.flush()
             t = time.time()
             lbp.lbp_image_his_8_2ji()
-            file.write(str(running) + ',8_2,no,' + str(time.time() - t) + '\n')
+            file.write(str(repetition) + ',' + str(running) + ',8_2,no,' + str(time.time() - t) + '\n')
             file.flush()
             t = time.time()
             lbp.lbp_image_his_16_2ji()
-            file.write(str(running) + ',16_2,no,' + str(time.time() - t) + '\n')
+            file.write(str(repetition) + ',' + str(running) + ',16_2,no,' + str(time.time() - t) + '\n')
             file.flush()
-    print('running N°',repetition)
+        print('running N°', repetition)
 print('finalice')
