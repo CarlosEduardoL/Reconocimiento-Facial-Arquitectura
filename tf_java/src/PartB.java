9c166794c9b407505b410c10d6449a46ec9b1231
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2019-11-28 13:10:29 -0500

    Ya casi acabamos

--- tf_java/src/PartB.java
+++ tf_java/src/PartB.java
@@ -15,44 +15,37 @@ public class PartB {
         Model[] carlos = getModels("Carlos", "data_set/1/");
         Model[] santiago = getModels("Santiago", "data_set/2/");
         Model[] nicolas = getModels("Nicolas", "data_set/3/");
-        List<Integer> index = Arrays.asList(0,1,2,3);
+        Detector detector = new Detector();
+        detector.addModel(nelson);
+        detector.addModel(carlos);
+        detector.addModel(santiago);
+        detector.addModel(nicolas);
         try(FileWriter fw = new FileWriter("funcionamiento.csv", true);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter out = new PrintWriter(bw))
         {
-            out.println("Replica,Tipo de algoritmo,Grupo,Respuesta,Acerto");
-            for (int replica = 1; replica <= 10; replica++) {
-                String r = replica+",";
-                List<Model> examples = new ArrayList<>();
-                Detector detector = new Detector();
-                fillDetector(index,examples,detector,nelson,carlos,santiago,nicolas);
-                for (Model face: examples) {
-                    String answer = detector.DetectLBP8_1(face);
-                    out.println(r + "8_1,"+face.getTag()+","+answer+","+face.getTag().equals(answer));
+            out.println("Tipo de algoritmo,Grupo,Sub grupo,Grupo Respuesta,Sub grupo respuesta,distancia chi");
+            Model[] samples = new Model[]{new Model("Nelson","test_set/0.jpeg",0),
+                                            new Model("Carlos","test_set/1.jpg",0),
+                                            new Model("Santiago","test_set/2.jpeg",0),
+                                            new Model("Nicolas","test_set/3.jpg",0) };
+            for (Model face: samples) {
+                String answer = detector.DetectLBP8_1(face);
+                out.println("8_1,"+face.getTag()+","+face.getSubTag()+","+answer);
 
-                    answer = detector.DetectLBP8_2(face);
-                    out.println(r + "8_2,"+face.getTag()+","+answer+","+face.getTag().equals(answer));
+                answer = detector.DetectLBP8_2(face);
+                out.println("8_2,"+face.getTag()+","+face.getSubTag()+","+answer);
 
-                    answer = detector.DetectLBP16_2(face);
-                    out.println(r + "16_2,"+face.getTag()+","+answer+","+face.getTag().equals(answer));
-                }
-                System.out.println("Replica #"+replica);
+                answer = detector.DetectLBP16_2(face);
+                out.println("16_2,"+face.getTag()+","+face.getSubTag()+","+answer);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void fillDetector(List<Integer> index, List<Model> examples, Detector detector, Model[]... models_) {
-        for (Model[] models : models_) {
-            Collections.shuffle(index);
-            examples.addAll(Arrays.asList(models[index.get(0)], models[index.get(1)]));
-            detector.addModel(models[index.get(2)], models[index.get(3)]);
-        }
-    }
-
     private static Model[] getModels(String name, String route) {
-        return Stream.of(0, 1, 2, 3).map(i -> new Model(name, route + i + (name.equals("Nelson")?".jpeg":".jpg"))).toArray(Model[]::new);
+        return Stream.of(0, 1, 2, 3).map(i -> new Model(name, route + i + (name.equals("Nelson")?".jpeg":".jpg"),i)).toArray(Model[]::new);
     }
 
 }
@@ -67,39 +60,44 @@ class Detector {
 
     void addModel(Model... models){
         this.models.addAll(Arrays.asList(models));
+        Collections.shuffle(this.models);
     }
 
     String DetectLBP8_1(Model face){
         double[] pesos = new double[models.size()];
-        double[] vectorFace = LBP.lbpHistogram8_1y2(face.getLbp().LBPImage_8_1_local());
+        double[] vectorFace = face.getHist8_1();
         int min = 0;
+        
         for (int i = 0; i < pesos.length; i++) {
-            pesos[i] = chiSquare(LBP.lbpHistogram8_1y2(models.get(i).getLbp().LBPImage_8_1_local()),vectorFace);
+            pesos[i] = chiSquare(models.get(i).getHist8_1(),vectorFace);
             if (pesos[i] < pesos[min]) min = i;
         }
-        return models.get(min).getTag();
+        return models.get(min).getTag()+","+models.get(min).getSubTag()+","+pesos[min];
     }
 
     String DetectLBP8_2(Model face){
         double[] pesos = new double[models.size()];
-        double[] vectorFace = LBP.lbpHistogram8_1y2(face.getLbp().LBPImage_8_2_local());
+        double[] vectorFace = face.getHist8_2();
         int min = 0;
+        
         for (int i = 0; i < pesos.length; i++) {
-            pesos[i] = chiSquare(LBP.lbpHistogram8_1y2(models.get(i).getLbp().LBPImage_8_2_local()),vectorFace);
+            pesos[i] = chiSquare(models.get(i).getHist8_2(),vectorFace);
             if (pesos[i] < pesos[min]) min = i;
         }
-        return models.get(min).getTag();
+        return models.get(min).getTag()+","+models.get(min).getSubTag()+","+pesos[min];
     }
 
     String DetectLBP16_2(Model face){
         double[] pesos = new double[models.size()];
-        double[] vectorFace = LBP.lbpHistogram16_2(face.getLbp().LBPImage_16_2_local());
+        double[] vectorFace = face.getHist16_2();
         int min = 0;
+        
         for (int i = 0; i < pesos.length; i++) {
-            pesos[i] = chiSquare(LBP.lbpHistogram16_2(models.get(i).getLbp().LBPImage_16_2_local()),vectorFace);
+            pesos[i] = chiSquare(models.get(i).getHist16_2(),vectorFace);
             if (pesos[i] < pesos[min]) min = i;
+            
         }
-        return models.get(min).getTag();
+        return models.get(min).getTag()+","+models.get(min).getSubTag()+","+pesos[min];
     }
 
     private double chiSquare(double[] x, double[] y){
@@ -116,18 +114,26 @@ class Detector {
 class Model{
 
     private String tag;
+    private int subTag;
     private LBP lbp;
+    private double[] hist8_1;
+    private double[] hist8_2;
+    private double[] hist16_2;
 
-    Model(String tag, String image){
+    Model(String tag, String image, int subTag){
         this.tag = tag;
+        this.subTag = subTag;
         try {
             lbp = new LBP(image);
+            hist8_1 = LBP.lbpHistogram8_1y2(lbp.LBPImage_8_1_local());
+            hist8_2 = LBP.lbpHistogram8_1y2(lbp.LBPImage_8_2_local());
+            hist16_2 = LBP.lbpHistogram16_2(lbp.LBPImage_16_2_local());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public String getTag() {
+    String getTag() {
         return tag;
     }
 
@@ -135,11 +141,27 @@ class Model{
         this.tag = tag;
     }
 
-    public LBP getLbp() {
+    LBP getLbp() {
         return lbp;
     }
 
     public void setLbp(LBP lbp) {
         this.lbp = lbp;
+    }
+
+    public double[] getHist8_1() {
+        return hist8_1;
+    }
+
+    public double[] getHist8_2() {
+        return hist8_2;
+    }
+
+    public double[] getHist16_2() {
+        return hist16_2;
+    }
+
+    public int getSubTag() {
+        return subTag;
     }
 }
